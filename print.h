ba6984cbd297f670cad3ca1fefd21799a76e450e
ericcar97 <eric.m.cardozo@gmail.com>
2022-12-07 19:24:15 -0300

    added ostream operator

--- print.h
+++ print.h
@@ -3,9 +3,33 @@
 
 #include<iostream>
 
+
+template<typename Type, size_type Size>
+std::ostream& operator<<(std::ostream& stream, const Array<Type,Size>& array){
+    stream << '[';
+    for(const auto & element : array){
+        stream << element << ',';
+    }
+    return stream << '\b' << ']' << std::endl;
+ }
+
+template<typename Type, size_type Rows, size_type Columns>
+std::ostream& operator<<(std::ostream& stream, const Matrix<Type,Rows,Columns>& matrix){
+    for(const auto & row : matrix){
+        stream << row;
+    }
+    return stream;
+ }
+
+
 template<typename ...Args>
 void print(Args&&... args){
     (std::cout << ... << args);
+}
+
+template<>
+void print(){
+    std::cout << std::endl;
 }
 
 #endif
---
+++ vector.h
@@ -0,0 +1,9 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include "dynamic_array.h"
+#include "static_array.h"
+
+
+
+#endif
